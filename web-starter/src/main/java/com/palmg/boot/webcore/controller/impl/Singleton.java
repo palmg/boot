8e11979463b6be919cbc0ac978e507723298a3af
jiony <jiony@126.com>
2018-03-08 18:39:38 +0800

    修改controller层返回信息体格式，增加一种自定义的信息体格式。

--- web-starter/src/main/java/com/palmg/boot/webcore/controller/impl/Singleton.java
+++ web-starter/src/main/java/com/palmg/boot/webcore/controller/impl/Singleton.java
@@ -15,6 +15,10 @@ public class Singleton implements Result {
 	private String msg;
 	private Object data;
 
+	public Singleton() {
+		
+	}
+	
 	public Singleton(int code, String msg, Object data) {
 		this.code = code;
 		this.msg = msg;
